f39f212738b4afd315ae5cd7bc57d9252d54cbdc
Haafii <hafismuhammedtk@gmail.com>
2023-10-26 18:12:00 +0530

    logger

--- freeCodeCamp/middleware/app.js
+++ freeCodeCamp/middleware/app.js
@@ -1,7 +1,23 @@
 const express = require('express')
 const app = express()
 
+const logger = (req, res, next) => {
+    const method = req.method
+    const url = req.url
+    const time = new Date().getFullYear()
+    console.log(method, url, time);
+    next()
+}
+app.get('/', logger, (req, res) => {
+    res.send('Home Page');
 
-app.listen(5009, ()=>{
+})
+
+app.get('/about',logger, (req, res) => {
+    res.send('About Page')
+
+})
+
+app.listen(5009, () => {
     console.log('server is listening on port 5009..');
 })
