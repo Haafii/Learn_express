6c6a4ffb24834bc24e44ae77b9ab8605c53c1082
Haafii <hafismuhammedtk@gmail.com>
2023-10-26 18:44:18 +0530

    authorize demo

--- freeCodeCamp/middleware/authorize.js
+++ freeCodeCamp/middleware/authorize.js
@@ -1,5 +1,11 @@
-const authorize= (req, res, next)=>{
-    console.log('authorize');
-    next()
+const authorize = (req, res, next) => {
+    const { user } = req.query;
+    if (user === 'hafi') {
+        req.user = { name: 'hafi', id: 5 }
+        next()
+    }
+    else {
+        res.status(401).send('Unauthorized')
+    }
 }
 module.exports = authorize;
