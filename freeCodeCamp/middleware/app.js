f741eeb42b73fb75c074136280992af9b68bbcef
Haafii <hafismuhammedtk@gmail.com>
2023-10-26 18:18:48 +0530

    logger middleware

--- freeCodeCamp/middleware/app.js
+++ freeCodeCamp/middleware/app.js
@@ -1,19 +1,17 @@
 const express = require('express')
 const app = express()
+const logger = require('./logger')
 
-const logger = (req, res, next) => {
-    const method = req.method
-    const url = req.url
-    const time = new Date().getFullYear()
-    console.log(method, url, time);
-    next()
-}
-app.get('/', logger, (req, res) => {
+
+app.use(logger)
+
+
+app.get('/', (req, res) => {
     res.send('Home Page');
 
-})
+}) 
 
-app.get('/about',logger, (req, res) => {
+app.get('/about', (req, res) => {
     res.send('About Page')
 
 })
